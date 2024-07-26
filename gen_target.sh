49f3594cc5c29ca20e75f6eea61c24166172cbe3
Azizbek <151017615+Azizbek17007@users.noreply.github.com>
2024-07-26 17:34:27 +0500

    123

--- gen_target.sh
+++ gen_target.sh
@@ -2,17 +2,24 @@
 
 config_file="config/prometheus/targets.yml"
 
+echo "Script started."
+
 if [ $# -lt 1 ] ; then
-  echo "Usage: $0 website-1.tld website-2.tld ..."
+  echo "Usage: $0 mudofaa.uz mail.mil.uz"
   exit 1
 fi
 
 if [ -f ${config_file} ] ; then
+  echo "Backing up existing config file."
   mv -v ${config_file} ${config_file}-$(date '+%s')
 fi
 
+echo "Creating new config file."
 echo "- targets:" > ${config_file}
 
 for x in $@ ; do
+  echo "Adding target: ${x}"
   echo "  - ${x}" >> ${config_file}
 done
+
+echo "Script finished."
